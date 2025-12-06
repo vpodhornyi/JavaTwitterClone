726a0a9df532630c14f75ee03cba819ac13c50b9
Viktor <vpodhornyi@gmail.com>
2025-12-06 23:11:45 +0100

    env refactoring

--- src/main/java/com/twitter/danit/config/WebSocketConfig.java
+++ src/main/java/com/twitter/danit/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.twitter.danit.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -9,10 +10,24 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+  @Value("${spring.rabbitmq.path}")
+  private String path;
+  @Value("${spring.rabbitmq.username}")
+  private String userName;
+  @Value("${spring.rabbitmq.password}")
+  private String userPassword;
+  @Value("${spring.rabbitmq.clientname}")
+  private String clientName;
+  @Value("${spring.rabbitmq.clientpassword}")
+  private String clientPassword;
+  @Value("${spring.rabbitmq.host}")
+  private String websocketHost;
+  @Value("${spring.rabbitmq.port}")
+  private int websocketPort;
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/ws")
+    registry.addEndpoint(path)
         .setAllowedOriginPatterns("*");
   }
 
@@ -21,11 +36,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     config
         .setApplicationDestinationPrefixes("/app")
         .enableStompBrokerRelay("/topic", "/queue")
-        .setSystemLogin("admin")
-        .setSystemPasscode("admin")
-        .setClientLogin("admin")
-        .setClientPasscode("admin")
-        .setRelayHost("twitter-rabbit")
-        .setRelayPort(61613);
+        .setSystemLogin(userName)
+        .setSystemPasscode(userPassword)
+        .setClientLogin(clientName)
+        .setClientPasscode(clientPassword)
+        .setRelayHost(websocketHost)
+        .setRelayPort(websocketPort);
   }
 }
